4976fef2977b9e2178b8e6c447d7f24586e14534
ialcaine <101673351+irenealcaine@users.noreply.github.com>
2023-09-18 12:06:46 +0000

    Update 3 files

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import MonsterPage from "./Pages/MonsterPage/MonsterPage";
 import Monsters from "./Pages/Monsters/Monsters";
+import Sets from "./Pages/Sets/Sets";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" exact element={<Monsters />} />
+          <Route path="/sets" exact element={<Sets />} />
           <Route path="/monsters/:id" element={<MonsterPage />} />
         </Routes>
       </BrowserRouter>
