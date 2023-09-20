b73b57c9d3e9357700ead51b8a274d07c0b70abf
ialcaine <101673351+irenealcaine@users.noreply.github.com>
2023-09-20 06:04:28 +0000

    Update SetPage.jsx

--- src/Pages/SetPage/SetPage.jsx
+++ src/Pages/SetPage/SetPage.jsx
@@ -1,5 +1,24 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 const SetPage = () => {
-  return <div className="header">Heeey</div>;
+  const [set, setSet] = useState([]);
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    fetch(`https://mhw-db.com/armor/sets/${id}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setSet(data);
+      });
+  }, [id, set.id]);
+
+  return (
+    <div className="header">
+      <div>{set.name}</div>
+    </div>
+  );
 };
 
 export default SetPage;
