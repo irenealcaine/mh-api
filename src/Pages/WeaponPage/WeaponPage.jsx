f2dc8e0420d49a3dbf9d34d791274c053ad9372e
ialcaine <101673351+irenealcaine@users.noreply.github.com>
2023-09-25 10:43:32 +0000

    Update WeaponPage.jsx

--- src/Pages/WeaponPage/WeaponPage.jsx
+++ src/Pages/WeaponPage/WeaponPage.jsx
@@ -1,7 +1,59 @@
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+
 const WeaponPage = () => {
+  const [weapon, setWeapon] = useState([]);
+  const [weapons, setWeapons] = useState([]);
+  const [previous, setPrevious] = useState([]);
+  const { id } = useParams();
+
+  useEffect(() => {
+    fetch(`https://mhw-db.com/weapons/${id}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setWeapon(data);
+      });
+
+    fetch(`https://mhw-db.com/weapons`)
+      .then((response) => response.json())
+      .then((weaponsData) => {
+        setWeapons(weaponsData);
+      });
+
+    setPrevious(weapons[weapon?.crafting?.previous - 1]);
+    console.log(previous);
+  }, [id, previous, weapons, weapon?.crafting?.previous]);
+
   return (
     <div className="weaponPage">
-      <span>weaponPage</span>
+      <h1>{weapon.name}</h1>
+      <p>Rarity: {weapon.rarity}</p>
+      <p>Damage: {weapon?.attack?.display}</p>
+      <p>Damage type: {weapon.damageType}</p>
+      {weapon?.elements?.length >= 1 && <p>Weapon elements:</p>}
+      {weapon?.crafting?.craftable ? (
+        <div>
+          <p>Crafting items:</p>
+          {weapon.crafting.craftingMaterials.map((material, index) => (
+            <Link to={`/items/${material.item.id}`}>
+              {material.item.name} x {material.quantity}
+            </Link>
+          ))}
+        </div>
+      ) : (
+        <div>
+          <p>Upgrade from:</p>
+          <Link to={`/weapons/${previous?.type}/${weapon?.crafting?.previous}`}>
+            {previous?.name}
+          </Link>
+          <p>Upgrade items:</p>
+          {weapon?.crafting?.upgradeMaterials.map((material, index) => (
+            <Link to={`/items/${material.item.id}`}>
+              {material.item.name} x {material.quantity}
+            </Link>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
