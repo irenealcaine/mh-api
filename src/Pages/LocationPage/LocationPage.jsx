67801ec5abeb91f316d86fcc90952f68a824ca6e
ialcaine <101673351+irenealcaine@users.noreply.github.com>
2023-09-25 06:08:14 +0000

    Update LocationPage.jsx

--- src/Pages/LocationPage/LocationPage.jsx
+++ src/Pages/LocationPage/LocationPage.jsx
@@ -1,28 +1,53 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 const LocationPage = () => {
-  const [location, setLocation] = useState([]);
+  const [locationData, setLocationData] = useState([]);
+  const [monsters, setMonsters] = useState([]);
 
   const { id } = useParams();
 
   useEffect(() => {
     fetch(`https://mhw-db.com/locations/${id}`)
       .then((response) => response.json())
-      .then((data) => {
-        setLocation(data);
-        console.log(data);
+      .then((locationData) => {
+        setLocationData(locationData);
       });
-  }, [id, location.id]);
+
+    fetch(`https://mhw-db.com/monsters`)
+      .then((response) => response.json())
+      .then((monstersData) => {
+        const filteredMonstersData = monstersData.filter((monster) => {
+          for (const location of monster.locations) {
+            if (location.name === locationData.name) {
+              return true;
+            }
+          }
+          return false;
+        });
+        setMonsters(filteredMonstersData);
+      });
+  }, [id, locationData.id, locationData.name]);
 
   return (
     <div className="locationPage">
-      <h1>{location.name}</h1>
-      {location?.camps?.map((camp, index) => (
-        <p key={index}>{camp.name}, zone: {camp.zone}</p>
+      <h1>{locationData.name}</h1>
+      {locationData?.camps?.map((camp, index) => (
+        <p key={index}>
+          {camp.name}, zone: {camp.zone}
+        </p>
       ))}
 
       <p>Monsters:</p>
+      {monsters.map((monsterItem, index) => (
+        <Link
+          className="monsterItem"
+          key={index}
+          to={`/monsters/${monsterItem.id}`}
+        >
+          {monsterItem.name}
+        </Link>
+      ))}
     </div>
   );
 };
