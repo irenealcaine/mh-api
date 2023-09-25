1802cb5ca82322dff2d502b7655f59bf761b5457
ialcaine <101673351+irenealcaine@users.noreply.github.com>
2023-09-25 06:13:51 +0000

    Update AilmentPage.jsx

--- src/Pages/AilmentPage/AilmentPage.jsx
+++ src/Pages/AilmentPage/AilmentPage.jsx
@@ -2,7 +2,8 @@ import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 
 const AilmentPage = () => {
-  const [ailment, setAilment] = useState([]);
+  const [ailmentData, setAilmentData] = useState([]);
+  const [monsters, setMonsters] = useState([]);
 
   const { id } = useParams();
 
@@ -10,24 +11,38 @@ const AilmentPage = () => {
     fetch(`https://mhw-db.com/ailments/${id}`)
       .then((response) => response.json())
       .then((data) => {
-        setAilment(data);
+        setAilmentData(data);
         console.log(data);
       });
-  }, [id, ailment.id]);
+
+    fetch(`https://mhw-db.com/monsters`)
+      .then((response) => response.json())
+      .then((monstersData) => {
+        const filteredMonstersData = monstersData.filter((monster) => {
+          for (const ailment of monster.ailments) {
+            if (ailment.name === ailmentData.name) {
+              return true;
+            }
+          }
+          return false;
+        });
+        setMonsters(filteredMonstersData);
+      });
+  }, [id, ailmentData.id, ailmentData.name]);
 
   return (
     <div className="ailmentPage">
-      <h1>{ailment.name}</h1>
-      <p>{ailment.description}</p>
+      <h1>{ailmentData.name}</h1>
+      <p>{ailmentData.description}</p>
       <h2>Recovery</h2>
       <p>Actions:</p>
-      {ailment?.recovery?.actions &&
-        ailment.recovery.actions.map((action, index) => (
+      {ailmentData?.recovery?.actions &&
+        ailmentData.recovery.actions.map((action, index) => (
           <span key={index}>{action}</span>
         ))}
       <p>Items:</p>
-      {ailment?.recovery?.items &&
-        ailment.recovery.items.map((item, index) => (
+      {ailmentData?.recovery?.items &&
+        ailmentData.recovery.items.map((item, index) => (
           <Link key={index} to={`/items/${item.id}`}>
             {item.name}
           </Link>
@@ -35,19 +50,30 @@ const AilmentPage = () => {
 
       <h2>Protection</h2>
       <p>Items:</p>
-      {ailment?.protection?.items &&
-        ailment.protection.items.map((item, index) => (
+      {ailmentData?.protection?.items &&
+        ailmentData.protection.items.map((item, index) => (
           <Link key={index} to={`/items/${item.id}`}>
             {item.name}
           </Link>
         ))}
       <p>Skills:</p>
-      {ailment?.protection?.skills &&
-        ailment.protection.skills.map((skill, index) => (
+      {ailmentData?.protection?.skills &&
+        ailmentData.protection.skills.map((skill, index) => (
           <Link key={index} to={`/skills/${skill.id}`}>
             {skill.name}
           </Link>
         ))}
+
+      <p>Monsters:</p>
+      {monsters.map((monsterItem, index) => (
+        <Link
+          className="monsterItem"
+          key={index}
+          to={`/monsters/${monsterItem.id}`}
+        >
+          {monsterItem.name}
+        </Link>
+      ))}
     </div>
   );
 };
