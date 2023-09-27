7672812eff8534fcc6a08dabf2f023ef0ad23f0d
ialcaine <101673351+irenealcaine@users.noreply.github.com>
2023-09-27 07:49:58 +0000

    Update 4 files

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,10 +2,12 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "../../assets/images/monsters/36.png";
 import "./Header.css";
-import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai"
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+import { homeItems } from "../../Utils/Constants";
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const menuActiveClass = isMenuOpen ? "active" : "";
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
@@ -17,31 +19,15 @@ const Header = () => {
         <Link to={`/`}>
           <img src={logo} className="logo" alt="logo" />
         </Link>
-        <div className={`menu-toggle ${isMenuOpen ? "active" : ""}`} onClick={toggleMenu}>
+        <div className={`menu-toggle ${menuActiveClass}`} onClick={toggleMenu}>
           {isMenuOpen ? <AiOutlineClose /> : <AiOutlineMenu />}
         </div>
-        <ul className={`nav-links ${isMenuOpen ? "active" : ""}`}>
-          <Link to={"/monsters"}>
-            <li>Monsters</li>
-          </Link>
-          <Link to={"/sets"}>
-            <li>Sets</li>
-          </Link>
-          <Link to={"/items"}>
-            <li>Items</li>
-          </Link>
-          <Link to={"/locations"}>
-            <li>Locations</li>
-          </Link>
-          <Link to={"/weapons"}>
-            <li>Weapons</li>
-          </Link>
-          <Link to={"/ailments"}>
-            <li>Ailments</li>
-          </Link>
-          <Link to={"/skills"}>
-            <li>Skills</li>
-          </Link>
+        <ul className={`nav-links ${menuActiveClass}`}>
+          {homeItems.map((homeItem) => (
+            <Link key={homeItem} to={`/${homeItem.name}`}>
+              <li>{homeItem.name}</li>
+            </Link>
+          ))}
         </ul>
       </nav>
     </div>
