bd2499ae775471ee205cb2875de01e3b0260d741
ialcaine <101673351+irenealcaine@users.noreply.github.com>
2023-09-28 09:18:07 +0000

    Update Items.jsx

--- src/Pages/Items/Items.jsx
+++ src/Pages/Items/Items.jsx
@@ -15,6 +15,15 @@ const Items = () => {
       });
   }, []);
 
+  const filteredItems = items.filter((item) => {
+    const nameMatch = item.name
+      .toLowerCase()
+      .includes(inputValue.toLowerCase());
+    const rarityMatch =
+      !selectValue || parseInt(item.rarity) === parseInt(selectValue);
+    return nameMatch && rarityMatch;
+  });
+
   return (
     <div className="itemsPage">
       <h1>Items</h1>
@@ -29,21 +38,10 @@ const Items = () => {
         <select
           onChange={(e) => setSelectValue(e.target.value)}
           className="input"
+          value={selectValue || ""}
         >
           <option value={""}>Rarity</option>
-          {/* <option value={1}>1</option>
-          <option value={2}>2</option>
-          <option value={3}>3</option>
-          <option value={4}>4</option>
-          <option value={5}>5</option>
-          <option value={6}>6</option>
-          <option value={7}>7</option>
-          <option value={8}>8</option>
-          <option value={9}>9</option>
-          <option value={10}>10</option>
-          <option value={11}>11</option>
-          <option value={12}>12</option> */}
-          {[...Array(13).keys()].map((value) => (
+          {[...Array(12).keys()].map((value) => (
             <option key={value} value={value + 1}>
               {value + 1}
             </option>
@@ -52,20 +50,11 @@ const Items = () => {
       </div>
 
       <div className="buttonContainer">
-        {items
-          .filter((item) =>
-            item.name.toLowerCase().includes(inputValue.toLowerCase()),
-          )
-          .filter((item) =>
-            selectValue
-              ? parseInt(item.rarity) === parseInt(selectValue)
-              : item,
-          )
-          .map((item, index) => (
-            <Link className="button" to={`/items/${item.id}`} key={index}>
-              {item.name}
-            </Link>
-          ))}
+        {filteredItems.map((item) => (
+          <Link className="button" to={`/items/${item.id}`} key={item.id}>
+            {item.name}
+          </Link>
+        ))}
       </div>
     </div>
   );
